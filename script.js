60387cd88fdf797126c201d6d7c346127133632d
Liz Emanuelle <164916177+lizzyswho@users.noreply.github.com>
2025-02-22 11:20:23 -0300

    ablubleblé

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// A estrutura DOM está no arquivo HTML inteiro, a propósito, é meio impossível não usá-la.
+
+// Usando o getElementById, as constantes abaixo constantemente, conseguimos, em tese, os elementos destacados com os seguintes Id:
+
 const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
+
+// Aqui temos um pequeno aviso legal, pois não lido bem com processos jurídicos e é melhor previnir do que remediar.
+window.alert('Não possuo qualquer direito sobre os materiais utilizados e o site tem unicamente como proposta facilitar o estudo e aprendizagem de HTML, CSS e JS.')
 
 function requestApi(searchTerm) {
     const url = `http://localhost:6600/artists?name_like=${searchTerm}`
@@ -8,6 +15,8 @@ function requestApi(searchTerm) {
         .then((response) => response.json())
         .then((result) => displayResults(result))
 }
+
+// O add e remove, logo abaixo, que salvo engano engano estudamos, adiciona e removem uma propriedade de um elemento.
 
 function displayResults(result) {
     resultPlaylist.classList.add("hidden")
